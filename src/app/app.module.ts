c7e5e1fd1e344d1ed1268fc5cb59be2876f632d6
sojeong2020 <macpark09@gmail.com>
2023-03-02 14:39:00 +0000

    start buiding structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginAdminComponent } from './core/components/login/login-admin/login-admin.component';
+import { LoginCoachComponent } from './core/components/login/login-coach/login-coach.component';
+import { LoginProviderComponent } from './core/components/login/login-provider/login-provider.component';
+import { ResetComponent } from './core/components/login/reset/reset.component';
+import { LoginFormComponent } from './core/components/shared/login-form/login-form.component';
+import { IndexComponent } from './feature/public/index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginAdminComponent,
+    LoginCoachComponent,
+    LoginProviderComponent,
+    ResetComponent,
+    LoginFormComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/core/components/login/login-coach/login-coach.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-coach',
+  templateUrl: './login-coach.component.html',
+  styleUrls: ['./login-coach.component.css']
+})
+export class LoginCoachComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
