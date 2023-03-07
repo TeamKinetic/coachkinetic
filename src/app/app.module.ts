bcbd3dc7bb028686265f01c8d6e645467cabaa37
sojeong2020 <macpark09@gmail.com>
2023-03-07 11:33:06 +0000

    add login components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-//import { LoginCoachComponent } from './core/components/login/login-coach/login-coach.component';
-//import { LoginProviderComponent } from './core/components/login/login-provider/login-provider.component';
-//import { ResetComponent } from './core/components/login/reset/reset.component';
-//import { LoginFormComponent } from './core/components/shared/login-form/login-form.component';
 import { IndexComponent } from './feature/public/index/index.component';
 import { CoreModule } from './core/core.module';
 
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './components/shared/header/header.component';
 import { LoginAdminComponent } from './components/login/login-admin/login-admin.component';
+import { LoginCoachComponent } from './components/login/login-coach/login-coach.component';
+import { LoginProviderComponent } from './components/login/login-provider/login-provider.component';
+import { ResetComponent } from './components/login/reset/reset.component';
 
+import { HeaderComponent } from './components/shared/header/header.component';
+import { LoginFormComponent } from './components/shared/login-form/login-form.component';
 
 @NgModule({
   declarations: [
+    LoginAdminComponent,
+    LoginCoachComponent,
+    LoginProviderComponent,
+    ResetComponent,
+    
     HeaderComponent,
-    LoginAdminComponent
+    LoginFormComponent
   ],
   imports: [
     CommonModule
