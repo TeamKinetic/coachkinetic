0de4ea42b1fd3fced44c7bdc9938b5348c770904
sojeong2020 <macpark09@gmail.com>
2023-03-09 15:40:32 +0000

    styling login components

--- src/app/shared/angular-material.module.ts
+++ src/app/shared/angular-material.module.ts
@@ -2,6 +2,9 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
@@ -10,11 +13,16 @@ import {MatCardModule} from '@angular/material/card';
   imports: [
     CommonModule,
     MatToolbarModule,
-    MatCardModule
+    MatCardModule,
+    MatFormFieldModule,
+    MatButtonModule,
   ],
   exports: [
     MatToolbarModule,
-    MatCardModule
+    MatCardModule,
+    MatFormFieldModule,
+    MatButtonModule,
+
   ]
 })
 export class AngularMaterialModule {}
